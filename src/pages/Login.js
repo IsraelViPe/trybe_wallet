c49a33051b680efd34e1ff9647cecc3622a8c09c
IsraelPereira <pereira.israel2070@gmail.com>
2022-09-29 18:23:20 -0300

    requisito 1 quase pronto

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,50 @@
 import React from 'react';
 
 class Login extends React.Component {
+  state = {
+    emailLocal: '',
+    passwordLocal: '',
+  };
+
+  handleChange = ({ target: { value, name } }) => {
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
-    return <div>Login</div>;
+    const { emailLocal, passwordLocal } = this.state;
+    const emailValidateRegex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/;
+    const minEnterPassword = 6;
+    return (
+      <div>
+        <form>
+          <input
+            onChange={ this.handleChange }
+            data-testid="email-input"
+            type="email"
+            name="emailLocal"
+            value={ emailLocal }
+            placeholder="E-mail"
+          />
+          <input
+            onChange={ this.handleChange }
+            data-testid="password-input"
+            type="password"
+            name="passwordLocal"
+            value={ passwordLocal }
+            placeholder="Senha"
+          />
+          <button
+            type="submit"
+            disabled={ !(emailValidateRegex.test(emailLocal)
+              && passwordLocal.length >= minEnterPassword) }
+          >
+            Entrar
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
